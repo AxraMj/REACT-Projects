0b7a5be756d225b84f59718c9b2f0e15add76b57
AxraMj <offxaxra@gmail.com>
2024-07-28 13:10:59 +0530

    Add files via upload

---
+++ Static Netflix Design/src/Components/RowPost/RowPost.js
@@ -0,0 +1,26 @@
+import React from 'react'
+import './RowPost.css'
+function RowPost() {
+    return (
+        <div className='row'>
+            <h2>Title</h2>
+            <div className='posters'>
+                <img className='poster' alt='poster' src='https://m.media-amazon.com/images/M/MV5BODI0ZTljYTMtODQ1NC00NmI0LTk1YWUtN2FlNDM1MDExMDlhXkEyXkFqcGdeQXVyMTM0NTUzNDIy._V1_FMjpg_UX1000_.jpg' />
+                <img className='poster' alt='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' />
+                <img className='poster' alt='poster' src='https://th.bing.com/th/id/OIP.2Mtg5bBD490duNnKl9x3kgHaK-?w=691&h=1024&rs=1&pid=ImgDetMain' />
+                <img className='poster' alt='poster' src='https://th.bing.com/th/id/OIP.cVilzzBymEZnwhkKkIv5NgHaKj?rs=1&pid=ImgDetMain' />
+                <img className='poster' alt='poster' src='https://www.mysteryrooms.in/wp-content/uploads/2020/06/Money-Heist-Poster1-min-1.jpg' />
+                <img className='poster' alt='poster' src='https://static.moviecrow.com/gallery/20200420/177921-4.jpg' />
+                <img className='poster' alt='poster' src='https://th.bing.com/th/id/OIP.772mw8bvUn5unGYZnVjgeAHaK-?w=1296&h=1920&rs=1&pid=ImgDetMain' />
+                <img className='poster' alt='poster' src='https://mlpnk72yciwc.i.optimole.com/cqhiHLc.IIZS~2ef73/w:auto/h:auto/q:75/https://bleedingcool.com/wp-content/uploads/2021/08/E8hQZCrVgAQgsEV.jpeg'/>
+                <img className='poster' alt='poster' src='https://th.bing.com/th/id/OIP.ZKbg59u1fWT2Cl5k46wcWQAAAA?w=421&h=600&rs=1&pid=ImgDetMain' />
+                <img className='poster' alt='poster' src='https://shop.mysteryrooms.in/wp-content/uploads/2020/06/Money-Heist-Poster1-scaled.jpg' />
+                <img className='poster' alt='poster' src='https://th.bing.com/th/id/OIP.qViNl__QNSVuZGQzTGnOEAHaLH?rs=1&pid=ImgDetMain' />
+                <img className='poster' alt='poster' src='https://i.pinimg.com/originals/38/2a/16/382a169ed83735947298c6612e7e4bd3.jpg' />
+                <img className='poster' alt='poster' src='https://images.squarespace-cdn.com/content/v1/59232e19579fb3fa44a693c2/1589212826160-UM9PEPGOS3OJPR0FJ81X/ke17ZwdGBToddI8pDm48kHZUaJeKzodyg_sXWBMxNTdZw-zPPgdn4jUwVcJE1ZvWQUxwkmyExglNqGp0IvTJZUJFbgE-7XRK3dMEBRBhUpxCBUU7B-_SAG1kGvCwYgmUjQXvn8_OJjtz3K1llMQBa1MPsuSXPSY3X-tgg78M7lI/SKOyqL1qFLIhbK6ho2lB-696x975.jpg?format=1500w' />
+            </div>
+        </div>
+    )
+}
+
+export default RowPost
