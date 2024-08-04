197a56ab4d36753825640c14f78b83739df64127
AxraMj <offxaxra@gmail.com>
2024-08-04 15:59:51 +0530

    Update constants.js

--- Netflix /src/constants/constants.js
+++ Netflix /src/constants/constants.js
@@ -1,3 +1,3 @@
 export const baseUrl= 'https://api.themoviedb.org/3';
-export const API_KEY="e0e0d8b5cc964b11285715d5eef3642e";
+export const API_KEY="";
 export const imageUrl="https://image.tmdb.org/t/p/original";
