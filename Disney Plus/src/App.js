6b01498f348f6d0a02ef96b9ec5d25a66ac8fd4c
AxraMj <offxaxra@gmail.com>
2024-08-08 18:46:08 +0530

    Add files via upload

---
+++ Disney Plus/src/App.js
@@ -0,0 +1,17 @@
+import {BrowserRouter as Router,Routes,Route} from "react-router-dom"
+import Login from './components/Login'
+import Header from "./components/Header";
+function App() {
+  return (
+   <div>
+      <Router>
+        <Header/>
+        <Routes>
+        <Route path='/' element={<Login />} />
+        </Routes>
+      </Router>
+   </div>
+  );
+}
+
+export default App;
